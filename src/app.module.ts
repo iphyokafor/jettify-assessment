c5ae24f295a080895053d8339436bdad43da041e
Okafor Ifeoma Sandra <sandraifeoma22@gmail.com>
2021-11-11 14:00:10 +0100

    Create users and wallets resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    UsersModule,
+    WalletsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/dto/base-user.dto.ts
@@ -0,0 +1,8 @@
+export class BaseUserDto {
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: number;
+  password: string;
+}
